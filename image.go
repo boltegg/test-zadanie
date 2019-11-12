c08aa8bd46c2d725f131e8cb0744acafb1334ab0
boltegg <boltegg@icloud.com>
2019-11-12 16:00:03 +0200

    add mysql

--- image.go
+++ image.go
@@ -1,36 +1,101 @@
 package main
 
 import (
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/sirupsen/logrus"
 	"io"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-//type ImageOriginal struct {
-//	Id int
-//	//UserId int
-//	Name string
-//	Resolution string
-//	//Path string
-//}
-//
-//type ImageResized struct {
-//	Id int
-//	OriginalId int
-//	Resolution string
-//	//Path string
-//}
-
-
-func SaveImage(imageRaw io.Reader, fileName string) (loc string, err error) {
-
-	// TODO: save info to db
-
-	// save image to aws
-	return saveFile(imageRaw, fileName)
+type ImageOriginal struct {
+	Id       int64
+	UserId   int64
+	FileName string
+	Format   string
 }
 
-func saveFile(file io.Reader, key string) (location string, err error) {
+type ImageResized struct {
+	Id         int64
+	UserId     int64
+	OriginalId int64
+	FileName   string
+	Format     string
+	Width      int
+	Height     int
+}
+
+func (i *ImageOriginal) Path() string {
+	return fmt.Sprintf("%d/%s_%d.%s", i.UserId, i.FileName, i.Id, i.Format)
+}
+
+func (i *ImageOriginal) Url() string {
+	return fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", AS3_BUCKET_NAME, AS3_REGION, i.Path())
+}
+
+func (i *ImageResized) Path() string {
+	return fmt.Sprintf("%d/%s_%d_%dx%d.%s", i.UserId, i.FileName, i.Id, i.Width, i.Height, i.Format)
+}
+
+func (i *ImageResized) Url() string {
+	return fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", AS3_BUCKET_NAME, AS3_REGION, i.Path())
+}
+
+//
+
+func SaveImage(imageRaw io.Reader, fileFullName string) (loc string, err error) {
+
+	fname, format, err := splitFileName(fileFullName)
+	if err != nil {
+		return
+	}
+
+	img := ImageOriginal{
+		UserId:   1,
+		FileName: fname,
+		Format:   format,
+	}
+
+	err = img.Insert()
+	if err != nil {
+		logrus.Error("DB Insert error:", err)
+		err = fmt.Errorf("Upload error")
+		return
+	}
+
+	_, err = uploadFileS3(imageRaw, img.Path())
+	if err != nil {
+		logrus.Error("S3 upload file error:", err)
+		err = fmt.Errorf("Upload error")
+		return
+	}
+
+	return
+}
+
+func (i *ImageOriginal) Insert() (err error) {
+
+	res, err := mysqlSess.Exec("INSERT INTO image_original (user_id, file_name, format) VALUES(?, ?, ?)", i.UserId, i.FileName, i.Format)
+	if err != nil {
+		return
+	}
+
+	i.Id, err = res.LastInsertId()
+	return
+}
+
+//
+
+func ResizeImage() {
+
+}
+
+//
+
+func uploadFileS3(file io.Reader, key string) (location string, err error) {
 
 	uploader := s3manager.NewUploader(s3sess)
 
@@ -38,10 +103,20 @@ func saveFile(file io.Reader, key string) (location string, err error) {
 		Bucket: aws.String(AS3_BUCKET_NAME),
 		Key:    aws.String(key),
 		Body:   file,
-		ACL: aws.String("public-read"),
+		ACL:    aws.String("public-read"),
 	})
 	if err != nil {
 		return
 	}
 	return result.Location, err
+}
+
+func splitFileName(fileName string) (name, format string, err error) {
+	slise := strings.Split(fileName, ".")
+	if len(slise) < 2 {
+		err = fmt.Errorf("File format unknown")
+	}
+	name = strings.Join(slise[:len(slise)-1], ".")
+	format = slise[len(slise)-1]
+	return
 }
--- main.go
+++ main.go
@@ -3,24 +3,30 @@ package main
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/jmoiron/sqlx"
 )
 
 const (
 	AS3_BUCKET_NAME = "mediaservice-test-zadanie-yalantis"
+	AS3_REGION = "eu-central-1"
 )
 
 var (
 	s3sess *session.Session
+	mysqlSess *sqlx.DB
 )
 
 func main() {
 
 	// init aws session
 	s3sess = session.Must(session.NewSession(&aws.Config{
-		Region: aws.String("eu-central-1")},
+		Region: aws.String(AS3_REGION)},
 	))
 
+	// init mysql session
+	mysqlSess = sqlx.MustConnect("mysql", "root:@tcp(localhost:3306)/test_zadanie")
 
+	// run http server
 	err := RunHttpServer(":80")
 	if err != nil {
 		panic(err)
